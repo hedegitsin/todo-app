797ca9e1b7bd4bb41c7870899515a483e1330549
Harun Yilmaz <harun.yilmaz@irdeto.com>
2023-05-30 11:11:37 +0200

    Converted branch 2 to separate Components

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {TodoItem} from "../../models/todo-item.model";
 
 
@@ -7,88 +7,14 @@ import {TodoItem} from "../../models/todo-item.model";
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit{
+export class TodoListComponent{
 
-  private todoList: Array<TodoItem> = [
-    {
-      id: 1,
-      todo: "My first TODO Item",
-      completed: false,
-      userId: 1
-    },
-    {
-      id: 2,
-      todo: "My second TODO Item",
-      completed: false,
-      userId: 1
-    },
-    {
-      id: 3,
-      todo: "My third TODO Item",
-      completed: false,
-      userId: 1
-    },
-    {
-      id: 4,
-      todo: "My fourth TODO Item",
-      completed: false,
-      userId: 1
-    },
-    {
-      id: 5,
-      todo: "My fifth TODO Item",
-      completed: false,
-      userId: 1
-    },
-    {
-      id: 6,
-      todo: "My sixth TODO Item",
-      completed: false,
-      userId: 1
-    },
-    {
-      id: 7,
-      todo: "My seventh TODO Item",
-      completed: false,
-      userId: 1
-    },
-    {
-      id: 8,
-      todo: "My eighth TODO Item",
-      completed: false,
-      userId: 1
-    },
-    {
-      id: 9,
-      todo: "My ninth TODO Item",
-      completed: false,
-      userId: 1
-    },
-    {
-      id: 10,
-      todo: "My tenth TODO Item",
-      completed: false,
-      userId: 1
-    }
-  ]
-
-  openTodoList: Array<TodoItem> = [];
-  completedTodoList: Array<TodoItem> = [];
-
-  ngOnInit(): void {
-    this.setFilteredTodoLists();
-  }
-
-  private filterTodoListByCompletion(isCompleted: boolean): Array<TodoItem> {
-    return this.todoList.filter(item => item.completed === isCompleted);
-  }
-
-  private setFilteredTodoLists() {
-    this.openTodoList = this.filterTodoListByCompletion(false);
-    this.completedTodoList = this.filterTodoListByCompletion(true);
-  }
+  @Input()
+  todoList: Array<TodoItem> = [];
 
   onTodoItemChecked(todoItem: TodoItem) {
-    this.setFilteredTodoLists();
+    if(todoItem.completed){
+      this.todoList = this.todoList.filter(item => item.id !== todoItem.id);
+    }
   }
 }
