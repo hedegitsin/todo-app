09497976a6b57b4597527a8c4c7817244a7553cc
Harun Yilmaz <harun.yilmaz@irdeto.com>
2023-05-31 19:05:05 +0200

    Added completion API calls

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -37,6 +37,8 @@ export class TodoListComponent implements OnInit {
   }
 
   onTodoItemChecked(todoItem: TodoItem) {
-    this.setFilteredTodoLists();
+    this.todoApiService.updateTodo(todoItem).subscribe({
+      next: () => this.setFilteredTodoLists()
+    })
   }
 }
--- src/app/services/todo-api.service.ts
+++ src/app/services/todo-api.service.ts
@@ -1,6 +1,7 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {TodoApiListResponse} from "../models/todo-api-list-response.model";
+import {TodoItem} from "../models/todo-item.model";
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +14,12 @@ export class TodoApiService {
   getAllTodos() {
     return this.httpClient.get<TodoApiListResponse>(this.BASE_API_URL)
   }
+
+  updateTodo(todoItem: TodoItem){
+    return this.httpClient.put<TodoApiListResponse>(
+      `https://dummyjson.com/todos/${todoItem.id}`,
+      JSON.stringify(todoItem)
+    )
+  }
+
 }
